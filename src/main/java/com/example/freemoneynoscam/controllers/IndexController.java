53833243e47417ac3267af1d1c62543ab6b85432
Nazmul-Huq <hmnazmulhuq@gmail.com>
2022-03-24 12:34:52 +0100

    controller all commented now

--- src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
+++ src/main/java/com/example/freemoneynoscam/controllers/IndexController.java
@@ -29,6 +29,11 @@ public class IndexController {
         return "index";
     }
 
+    // redirect to homepage
+    @GetMapping("/redirect-to-homepage")
+    public String redirectToHomepage(){
+        return "index";
+    }
 
     // map to add email page
     @GetMapping("/add-email")
@@ -36,29 +41,25 @@ public class IndexController {
         return "add-email";
     }
 
+    // map to the page to show a single email
     @GetMapping("/get-a-random-email")
     public String getARandomEmail(Model model){
         model.addAttribute("emails", manageData.getSingleEmailFromDatabase());        ;
         return "show-email";
     }
 
+    // map to the page to show all email
     @GetMapping("/get-all-email")
     public String getAllEmail(Model model){
         model.addAttribute("emails", manageData.getAllEmailFromDatabase());        ;
         return "show-email";
     }
 
-
+    // post mapping to submit form data (add a email to the database)
     @PostMapping("/test")
     public String addEmail(WebRequest formData){
         String succeedOrNot = validateEmailService.addEmailToDatabase(formData.getParameter("email"));
         return "redirect:/"+succeedOrNot;
-    }
-
-    // page to show after successfully added to the database
-    @GetMapping("/succeed")
-    public String succeed(){
-        return "succeed";
     }
 
     // page to show if email validation failed
@@ -67,16 +68,16 @@ public class IndexController {
         return "invalid-email";
     }
 
-    // page to if email is valid but failed to add to database for connection or other issue(s)
+    // page to show if email is valid but failed to add to database for connection or other issue(s)
     @GetMapping("/not-succeed")
     public String notSucceed(){
         return "not-succeed";
     }
 
-    // redirect to homepage
-    @GetMapping("/redirect-to-homepage")
-    public String redirectToHomepage(){
-        return "index";
+    // page to show after email added successfully to the database
+    @GetMapping("/succeed")
+    public String succeed(){
+        return "succeed";
     }
 
-}
+} // end of class
